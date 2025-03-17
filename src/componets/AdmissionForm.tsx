d63d82697884d267b16b6421786c5a7d3e989f40
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-03-17 11:17:05 +0530

    dynamic tracking code

--- src/componets/AdmissionForm.tsx
+++ src/componets/AdmissionForm.tsx
@@ -12,6 +12,7 @@ const AdmissionForm: React.FC = () => {
       document.body.removeChild(script);
     };
   }, []);
+  
   return (
     <div className="w-80 sm:w-100 mx-auto h-[500px] p-4 pt-6 bg-white rounded-lg shadow-md" >
       <div className="npf_wgts" data-height="450px" data-w="b214bb41b7025009877617366f69b11c"></div>
@@ -20,6 +21,8 @@ const AdmissionForm: React.FC = () => {
 };
 
 export default AdmissionForm;
+
+
 
 
 
